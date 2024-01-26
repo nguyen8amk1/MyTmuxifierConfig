8e0b8f06b3dc46e8b9881f342851e59e8ddada9d
Nguyễn Trần Trung Nguyên <trungnguyen123akohshit@gmail.com>
2024-01-26 14:15:43 +0700

    tmuxifier

---
+++ layouts/philo.session.sh
@@ -0,0 +1,20 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+#
+session_root "~/Documents/GitHub/My_Own_Philosophy_of_Everything"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "philo"; then
+
+  # Create a new window inline within session layout definition.
+  new_window "philo"
+  # Load a defined window layout.
+  # load_window "example"
+  # Select the default active window on session creation.
+  # select_window 1
+  run_cmd "~/./nvim ."
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
